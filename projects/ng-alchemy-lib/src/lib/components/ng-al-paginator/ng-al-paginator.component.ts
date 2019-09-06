ebcae328dc0015473d115037bd60322ba0716b13
Cesar Armando Perez Fernandez <E1007139@ercbpo.com>
2019-09-06 16:42:35 -0400

    Adding paginator visually.

--- projects/ng-alchemy-lib/src/lib/components/ng-al-paginator/ng-al-paginator.component.ts
+++ projects/ng-alchemy-lib/src/lib/components/ng-al-paginator/ng-al-paginator.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'ng-al-dpaginator',
-    templateUrl: './ng-al-paginator.component.html',
-    styleUrls: ['./ng-al-paginator.component.scss']
+	selector: 'ng-al-dpaginator',
+	templateUrl: './ng-al-paginator.component.html',
+	styleUrls: ['./ng-al-paginator.component.scss']
 })
 export class NgAlPaginatorComponent {
-
+	public fakeBtns = [5, 6, 7, 8, 9];
 }
